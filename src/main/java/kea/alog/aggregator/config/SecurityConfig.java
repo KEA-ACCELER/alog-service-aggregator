42f99733117a88e2f08f78ce13939b60507d901a
Yejin Choi <63734765+chldppwls12@users.noreply.github.com>
2023-08-13 00:07:02 +0900

    hotfix: cors

---
+++ src/main/java/kea/alog/aggregator/config/SecurityConfig.java
@@ -0,0 +1,60 @@
+package kea.alog.aggregator.config;
+
+import java.util.Arrays;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Configuration
+@RequiredArgsConstructor
+public class SecurityConfig {
+    private static final String[] AUTH_WHITELIST = {
+        // -- Swagger UI v3
+        "/v3/api-docs/**",
+        "v3/api-docs/**",
+        "/swagger-ui/**",
+        "swagger-ui/**",
+        "/api/aggr/**"
+    };
+
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.cors()
+            .and()
+            .csrf().disable()   // token 사용 방식이라 csrf disable
+            .exceptionHandling()
+            .and()
+            .headers()
+            .frameOptions()
+            .sameOrigin()
+            .and()  // session 사용하지 않기에 STATELESS
+            .sessionManagement()
+            .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            .and()
+            .authorizeHttpRequests(requests -> requests
+                .requestMatchers(AUTH_WHITELIST).permitAll()
+                .anyRequest().authenticated()
+            );
+
+        return http.build();
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(
+            Arrays.asList("*"));
+        configuration.setAllowedMethods(Arrays.asList("*"));
+        configuration.setAllowedHeaders(Arrays.asList("*"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+}
